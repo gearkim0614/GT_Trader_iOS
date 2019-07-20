4799cfa5e070d502a8e43a90ed0c785406907eab
Kibeom Kim <gearkim0614@gmail.com>
2019-07-19 22:31:38 -0400

    working player check

--- GT_Trader_iOS/ViewController.swift
+++ GT_Trader_iOS/ViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    //var player: Player?
+    
+    let difficulty = ["Beginner", "Easy", "Normal", "Hard", "Impossible"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return difficulty[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return difficulty.count
+    }
+    
+    func createPlayer(name: String, pilot: Int, fighter: Int, trader: Int, engineer: Int) -> Bool {
+        
+        if pilot + fighter + trader + engineer == 16 && name != "" {
+            //self.player = Player(name: "name", pilot: 4, trader: 4, fighter: 4, engineer: 4)
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,6 +52,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var FighterLabel: UILabel!
     
     @IBOutlet weak var TraderLabel: UILabel!
+    
+    @IBOutlet weak var pickerView: UIPickerView!
     
     @IBAction func PilotSlider(_ sender: UISlider) {
         PilotLabel.text = String(Int(sender.value))
@@ -40,8 +70,20 @@ class ViewController: UIViewController {
         TraderLabel.text = String(Int(sender.value))
     }
     
+    
 
     @IBAction func createPlayer(_ sender: UIButton) {
+        if let name = nameField.text, let pilotVal = Int(PilotLabel.text!), let fighterVal = Int(FighterLabel.text!), let traderVal = Int(TraderLabel.text!), let engineerVal = Int(EngineerLabel.text!) {
+            if createPlayer(name: name, pilot: pilotVal, fighter: fighterVal, trader: traderVal, engineer: engineerVal) {
+                //self.performSegue(withIdentifier: "createPlayer", sender: self)
+            } else {
+                let alert = UIAlertController(title: "Invalid Input", message: "Please type in a name and make sure your points add up to 16", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            
+        }
+        
         
     }
     
